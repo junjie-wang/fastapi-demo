102073102f512f99a8401285f360f57315a5d592
wangjunjie <wjj900906@126.com>
2023-08-01 14:57:34 +0800

    第五六七章视频学习

---
+++ coronavirus/main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# __author__ = '__Jack__'
+
+from typing import List
+
+import requests
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks, Request
+from fastapi.templating import Jinja2Templates
+from pydantic import HttpUrl
+from sqlalchemy.orm import Session
+
+from coronavirus import crud, schemas
+from coronavirus.database import engine, Base, SessionLocal
+from coronavirus.models import City, Data
+
+application = APIRouter()
+
+templates = Jinja2Templates(directory='./coronavirus/templates')
+
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@application.post("/create_city", response_model=schemas.ReadCity)
+def create_city(city: schemas.CreateCity, db: Session = Depends(get_db)):
+    db_city = crud.get_city_by_name(db, name=city.province)
+    if db_city:
+        raise HTTPException(status_code=400, detail="City already registered")
+    return crud.create_city(db=db, city=city)
+
+
+@application.get("/get_city/{city}", response_model=schemas.ReadCity)
+def get_city(city: str, db: Session = Depends(get_db)):
+    db_city = crud.get_city_by_name(db, name=city)
+    if db_city is None:
+        raise HTTPException(status_code=404, detail="City not found")
+    return db_city
+
+
+@application.get("/get_cities", response_model=List[schemas.ReadCity])
+def get_cities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    cities = crud.get_cities(db, skip=skip, limit=limit)
+    return cities
+
+
+@application.post("/create_data", response_model=schemas.ReadData)
+def create_data_for_city(city: str, data: schemas.CreateData, db: Session = Depends(get_db)):
+    db_city = crud.get_city_by_name(db, name=city)
+    data = crud.create_city_data(db=db, data=data, city_id=db_city.id)
+    return data
+
+
+@application.get("/get_data")
+def get_data(city: str = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    data = crud.get_data(db, city=city, skip=skip, limit=limit)
+    return data
+
+
+@application.get("/")
+def coronavirus(request: Request, city: str = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    data = crud.get_data(db, city=city, skip=skip, limit=limit)
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "data": data,
+        "sync_data_url": "/coronavirus/sync_coronavirus_data/jhu"
+    })
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
-from tutorial import app03, app04, app05
+from tutorial import app03, app04, app05, app06, app07
+from coronavirus import application
 from fastapi.staticfiles import StaticFiles
 
 # from fastapi.exceptions import RequestValidationError
@@ -43,6 +44,9 @@ app.mount(path='/static', app=StaticFiles(directory='./coronavirus/static'),
 app.include_router(app03, prefix='/chapter03', tags=['第三章 请求参数和验证'])
 app.include_router(app04, prefix='/chapter04', tags=['第四章 响应处理和FastAPI配置'])
 app.include_router(app05, prefix='/chapter05', tags=['第五章 FastAPI的依赖注入系统'])
+app.include_router(app06, prefix='/chapter06', tags=['第六章 安全认证和授权'])
+app.include_router(app07, prefix='/chapter07', tags=['第七章 数据库操作和多应用的目录结构设计'])
+app.include_router(application, prefix='/coronavirus', tags=['新冠病毒疫情跟踪器API'])
 
 if __name__ == '__main__':
     uvicorn.run('run:app', host='0.0.0.0', port=8000, reload=True, debug=True, workers=1)
